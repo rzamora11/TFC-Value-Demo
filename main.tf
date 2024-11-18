3879acbaeaa33b36ea805fcbe8ffcffb4ee146eb
Román Zamora <roman.zamora@roman.zamora-HT4RW0K91R>
2024-11-18 12:22:49 +0100

    .

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ provider "aws" {
 /* AWS  */
 resource "aws_instance" "app_server" {
   ami           = "ami-0ef9e689241f0bb6e"
-  instance_type = "t2.micro"
+  instance_type = "e2.large"
 
   vpc_security_group_ids = [aws_security_group.example_security_group.id]
   
